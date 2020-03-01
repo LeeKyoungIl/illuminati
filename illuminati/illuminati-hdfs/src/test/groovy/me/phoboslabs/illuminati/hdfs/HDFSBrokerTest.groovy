c5cd515d82164d99d8bee24d88c13a120ff02ebd
leekyoungil <leekyoungil@gmail.com>
2020-03-01 14:02:19 +0900

    add append function

--- illuminati/illuminati-hdfs/src/test/groovy/me/phoboslabs/illuminati/hdfs/HDFSBrokerTest.groovy
+++ illuminati/illuminati-hdfs/src/test/groovy/me/phoboslabs/illuminati/hdfs/HDFSBrokerTest.groovy
@@ -8,7 +8,7 @@ class HDFSBrokerTest extends Specification {
     private DataBroker dataBorker;
 
     def setup() {
-        HDFSConnectionInfo hdfsConnectionInfo = new HDFSConnectionInfo("your hdfs ip address", 8020, "hdfs");
+        HDFSConnectionInfo hdfsConnectionInfo = new HDFSConnectionInfo("10.106.164.50", 8020, "hdfs");
         hdfsConnectionInfo.hdfsSecurityAuthentication = "SIMPLE";
         hdfsConnectionInfo.hdfsSecurityAuthorization = "FALSE";
         hdfsConnectionInfo.homeDir = "/";
@@ -41,7 +41,7 @@ class HDFSBrokerTest extends Specification {
 
     def "TEST : delete the file in hadoop"() {
         setup:
-        String source = "/tmp/test2.txt";
+        String source = "/tapas-ocr/receipt-info";
 
         when:
         boolean deleteResult = this.dataBorker.deleteFile(source, true);
